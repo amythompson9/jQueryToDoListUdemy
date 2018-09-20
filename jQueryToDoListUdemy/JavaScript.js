98e9fa7f0227137fa2816170779fd8c308738f05
Amy Thompson <amythompson9@gmail.com>
2018-09-20 19:18:37 -0400

    Toggle on/off

--- jQueryToDoListUdemy/JavaScript.js
+++ jQueryToDoListUdemy/JavaScript.js
@@ -1,1 +1,27 @@
-﻿
+﻿//Check off specific todo by clicking
+
+$("li").click(function () {
+    $(this).toggleClass("completed");
+});
+
+//the below iis a lot of code and logic
+//so we just added a completed class to css
+//and made the code above
+
+
+//$("li").click(function () {
+//    //if li is gray
+//    if ($(this).css("color") === "rgb(128, 128, 128)") {
+//        //turn it black
+//        $(this).css({
+//            color: "black",
+//            textDecoration: "none",
+//        });
+//    }
+//    else {
+//        $(this).css({
+//            color: "gray",
+//            textDecoration: "line-through",
+//        });
+//    }
+//});

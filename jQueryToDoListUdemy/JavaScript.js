d276759e15f37b82f732ffaf7051e76537f081c7
Amy Thompson <amythompson9@gmail.com>
2018-09-21 23:18:53 -0400

    Add new items, delete

--- jQueryToDoListUdemy/JavaScript.js
+++ jQueryToDoListUdemy/JavaScript.js
@@ -1,10 +1,16 @@
 ﻿//Check off specific todo by clicking
+//$("li").click(function () {
+//    $(this).toggleClass("completed");
+//});
 
-$("li").click(function () {
+//above is old code that wouldn't work when a new item is
+//added to the list
+//below uses on() so that a new item can still be deleted
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
-//the below iis a lot of code and logic
+//the below is a lot of code and logic
 //so we just added a completed class to css
 //and made the code above
 
@@ -28,9 +34,19 @@ $("li").click(function () {
 
 //Click on X to delete To-Do
 
-$("span").click(function (event) {
+$("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        //grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
